40e4c880b08917f824ec4f6c971617895034a706
Malong <miss_longma@163.com>
2016-03-29 09:10:45 +0800

    评测程序

--- crfchinese/pingce.py
+++ crfchinese/pingce.py
@@ -26,17 +26,34 @@ def pingce(filename,new_file_name):
             s1=s2=s3=''
     f1.close()
     f2.close()
-pingce('G:/cresult-3','G:/cresult-3-p')
+# pingce('G:/cresult-3','G:/cresult-3-p')
 
 # 查找列表中连续的B，I
-def find_tag(s,tags):
-    flag = []
-    for i in range(len(s)):
-        flag.append(0)
-    for i in range(len(s)):
-        if tags[i].startswith('B'):
-            flag[i]=2   # B:2; I1,I1:1; I1,I2:3
-        if
+def find_tag(s,tag):  # s: sentence list , tags: tag list
+    slot_vals = []
+    map = {}
+    i=0
+    # print s
+    # print "len s is ;",len(s)
+    # print "tag list is:",tag
+    while(i<len(s)):
+        if tag[i].startswith(u'B'):
+            slot_name = tag[i].split(u'_')[-1]
+            slot_val = s[i]
+            i = i + 1
+            while(i < len(s)):
+                if tag[i].startswith(u'I') and tag[i].split(u'_')[-1] == slot_name:
+                    slot_val=slot_val+s[i]
+                    i=i+1
+                else:
+                    break
+            print slot_val
+        else:
+         i=i+1
+
+
+
+
 
 
 
@@ -44,24 +61,33 @@ def find_tag(s,tags):
 
 def pingce2(filename, new_file_name):
     f = open(filename)
-    f1 = open(new_file_name,'w')
+    f1 = open(new_file_name, 'w')
     line_list = f.readlines()
     for index in range(len(line_list)):
-        line_list[index] = line_list[range(index)].decode('utf-8')
-    for index in range(len(line_list)-2):
-        if len(line_list[index])>0:
+        line_list[index] = line_list[index].decode('utf-8')
+    index=0
+    while(index<len(line_list)):
+        print "index,",index
+        if len(line_list[index].strip())>0:
+            print"s index: ", line_list[index]
+            print "s index+1 ", line_list[index+1]
+            print line_list[index+2]
             f1.write(line_list[index].encode('utf-8'))
             f1.write(line_list[index+1].encode('utf-8'))
             f1.write(line_list[index+2].encode('utf-8'))
-            sentence = line_list[index].split()
-            tag = line_list[index+1].split()
-            gold = line_list[index+2].split()
+            sentence = line_list[index].strip().split(' ')
+            tag = line_list[index+1].strip().split(' ')
+            gold = line_list[index+2].strip().split(' ')
+            if len(sentence)>0:
+                find_tag(sentence,tag)
+                find_tag(sentence,gold)
+            index = index + 3
+        else:
+            index=index+1
+    f.close()
+    f1.close()
 
-            for i in range(len(sentence)):
-                if tag[i].startswith('B'):
-                    slot_name = tag[i].split('_')[-1]
-                    slot_val = tag[i]
-                    for k in range(len(sentence)-i):
+pingce2("D:/GitHub/graduate/cresult-3-p",'D:/GitHub/graduate/cresult-3-p1')
 
 
 
